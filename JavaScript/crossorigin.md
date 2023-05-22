7af0517ea54e65810724a9b0200ed36831d6432e
proNeel02 <prashantdabe02@gmail.com>
2023-05-22 13:12:33 +0530

    cors

--- JavaScript/crossorigin.md
+++ JavaScript/crossorigin.md
@@ -1,1 +1,3 @@
->>
+# What is CORS(cross Origin Resource Sharing) ?
+
+![Diagram Representation Of CORS](https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS/cors_principle.png)
---
+++ React/App.js
@@ -0,0 +1,13 @@
+
+const heading = React.createElement('h1', {}, "Hello React!!");
+const child = React.createElement('div', {id:'child'}, heading);
+const parent = React.createElement('div', {id:'parent'}, child);
+
+
+const element = React.createElement("h1", {}, "Hello from React");
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+console.log(root.render(parent));
